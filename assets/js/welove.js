12e4f64aeaee07303d30f0d2e767b505913c4341
thinktouch <wenshifan@gmail.com>
2016-01-03 22:52:42 +0800

    window onresize, location reload

--- assets/js/welove.js
+++ assets/js/welove.js
@@ -5,7 +5,11 @@ var initImgs = 30;/* 初始化总需要加载的图片数*/
 var imgLoadCount = 0; /* 这个是计数器，现在这个计数器是0 */
 
 window.onload = function(){
-    initData(initImgs);
+   loadStart();
+};
+
+function loadStart(){
+	initData(initImgs);
     var onceScroll = 10;
     var onceScrollBase = 31;
     var scrollEnd = false;
@@ -43,7 +47,11 @@ window.onload = function(){
             }, 100)
         }
     }
-};
+}
+
+window.onresize = function () {
+	location.reload();
+}
 
 // 初始化数据
 function initData(initImgs){
@@ -99,6 +107,7 @@ function waterfall(parent,box){
     //计算整个页面显示的列数(页面宽/box的宽)
     var oBoxW = oBoxs[0].offsetWidth;
     var cols = Math.floor(document.documentElement.clientWidth/oBoxW);
+
     // 设置main的宽度
     oParent.style.cssText = 'width:' + oBoxW * cols + "px;margin:0 auto";
     var hArr = []; // 存放每一列高度的数组
@@ -110,8 +119,10 @@ function waterfall(parent,box){
             var minhIndex = getMinhIndex(hArr, minH);
             oBoxs[i].style.position = 'absolute';
             oBoxs[i].style.top = minH + 'px';
-            oBoxs[i].style.left = oBoxW * minhIndex + 'px';
-            //oBoxs[i].style.left = oBoxs[minhIndex].offsetLeft + 'px';
+            //oBoxs[i].style.left = oBoxW * minhIndex + 'px';
+            oBoxs[i].style.left = oBoxs[minhIndex].offsetLeft + 'px';
+    		
+
             hArr[minhIndex] += oBoxs[i].offsetHeight;
         }
     }
